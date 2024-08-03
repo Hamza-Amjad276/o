77b4d914f69982c1f97ba08104b5d4e9c44576cb
Hamza Amjad <hamzaharis298@gmail.com>
2024-08-03 16:40:57 +0500

    Add files via upload

---
+++ New folder (4)/app.ts
@@ -0,0 +1,183 @@
+
+// // let student = {
+// //     name : "hamza",
+// //     class : 2,
+// //     grade : 9,
+// // }
+// // console.log(student.name);
+
+// // let student1 = {
+// //     name : string,
+// //     age : number,
+// //     class : number,
+
+// // } = {
+// //     name : "hamza"
+// //     age : 15
+// //     class : 8
+// // }
+
+// type std = {
+//    name : string,
+//    age : number,
+//    class: number,
+
+// }
+
+// let std50 : std = {
+//     name : "hamza",
+//     age : 15,
+//     class : 9,
+
+
+// }
+
+// let std51 : std = {
+//     name : "ali",
+//     age : 12,
+//     class : 6,
+
+
+// }
+
+
+// //union
+
+// let fname : String , number , boolean
+// fname = "hamza"
+// fname = 15
+// fname = false
+
+// let trafficlights : 'red' , 'green' , 'yellow'
+// trafficlights  = `red` 
+// trafficlights = `green`
+// trafficlights = `yellow`
+
+// intersection 
+
+// type teacher = {
+//     name : string
+//     facid : number
+//     salary : number
+//     position : string
+
+// }
+
+
+// type std = {
+//    name : string,
+//    age : number,
+//    class: number,
+
+// }
+
+// let person : teacher & std = {
+//     name : "maryam",
+//     facid : 4,
+//     salary : 10000,
+//     position : "teacher",
+//     age : 15,
+//     class : 9,
+// }
+
+// Logic statement 
+// if else
+// else if
+// nested if else
+// ternory operators
+// switch case
+// var marks = 75;
+// if (marks >= 90 && marks <= 100) {
+//     console.log("Grade : A1+ ");
+// }
+// else if (marks >= 80 && marks <= 89) {
+//     console.log("grade A+ ");
+// }
+// else if (marks >= 70 && marks <= 79) {
+//     console.log("grade : A ");
+// }
+// else if (marks >= 60 && marks <= 69) {
+//     console.log("grade : B");
+// }
+// else if (marks >= 50 && marks <= 59) {
+//     console.log("grade ; C");
+// }
+// else if (marks >= 40 && marks <= 49) {
+//     console.log("grade : D");
+// }
+// else {
+//     console.log("Fail");
+// }
+// var day = "monday";
+// if (true) {
+//     if (day === "monday") {
+//         console.log("Go to the work.");
+//     }
+//     else {
+//         console.log("today is sunday");
+//     }
+//     if (day === "tuesday") {
+//         console.log("this is second of week");
+//     }
+//     else {
+//         console.log("today is a hot day");
+//     }
+// }
+// var raining = true; // barish ho rahi hai
+// if (raining = true) {
+//     console.log("wear a rain cot.");
+// }
+// else {
+//     console.log("wear a sun glasses.");
+// }
+// var weather = "sunny";
+// if (weather === "raining") {
+//     console.log("wear a rain cot.");
+// }
+// else if (weather === "sunny") {
+//     console.log("wear a sun glasses.");
+// }
+// else {
+//     console.log("weather is cloudy");
+// }
+// var calculator;
+// if (calculator === undefined) {
+//     var num1 = 56;
+//     var num2 = 78;
+//     var num3 = num1 + num2;
+//     console.log(`the sum of ${num1} and ${num2} is ${num3}`)
+// }
+// else {
+//     console.log("invalid number");
+// }
+// let myname = "hamza"
+// console.log(myname);
+// setTimeout(() => {
+//     console.log(myname)
+// },7000);
+// let myage = 45
+// console.log("myage");
+// setTimeout(() => {
+//     console.log("myage")
+// },5000);
+// let myname1 = "ali"
+// console.log("myname1");
+// setTimeout(() => {
+//     console.log("myname1")
+// },1000);
+// function order(){
+//     setTimeout(() => {
+//         console.log("please wait");
+        
+//     },4000);
+// }
+// order()
+// //call back
+
+//  function prepare() {
+//     setTimeout(() => {
+//        console.log("please wait to pizza");
+        
+//     }, 4000);
+//  }
+ 
